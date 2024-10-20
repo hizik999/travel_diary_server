13277053472e655f67f8b08003a0065cac1c37e6
MalyshevAM <com.hizik@gmail.com>
2024-10-20 22:19:00 +0300

    начало обучения: создал фейковую БД

---
+++ db.py
@@ -0,0 +1,22 @@
+class Database:
+    _tickets = [
+        {
+            'id': 1,
+            'title': 'Ticket 1',
+            'status': 'In progress',
+        },
+        {
+            'id': 2,
+            'title': 'Ticket 2',
+            'status': 'Ready'
+        },
+        {
+            'id': 3,
+            'title': 'Ticket 3',
+            'status': 'Draft'
+        }   
+    ]
+
+    @property
+    def tickets(self):
+        return self._tickets
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+import uvicorn
+from db import Database
 
 
 app = FastAPI()
 
 
 @app.get("/")
-def hello_world():
-    return {"message": "Hello World"}
+def home():
+    return { "message": f"Home page" }
+
+
+@app.get("/tickets")
+def tickets():
+    db = Database()
+    return db.tickets
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
