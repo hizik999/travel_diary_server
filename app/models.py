3b43144e5876c501853e8c85bb626b5bb7464e52
MalyshevAM <com.hizik@gmail.com>
2024-11-02 13:32:46 +0300

    рефактор schemas и models

--- app/models.py
+++ app/models.py
@@ -1,38 +1,32 @@
 # schema.py
-from pydantic import BaseModel, Field, conint, validator
+from pydantic import BaseModel, Field, validator
 from typing import Optional
 
-class Coarse(BaseModel):
-    id: conint(ge=0, le=1000)  # Ограничение на диапазон ID от 0 до 8
-    name: str = Field(..., max_length=50)
+class Label(BaseModel):
+    id: int = Field(..., ge=0, le=8, description="Label ID")  # Ограничение на диапазон ID от 0 до 8
+    name: str = Field(..., max_length=25, description="Label name")
 
     class Config:
         orm_mode = True  # Позволяет работать с объектами SQLAlchemy напрямую
 
 class Motion(BaseModel):
-    time: int = Field(..., description="Unix timestamp")
-    acceleration_x: float
-    acceleration_y: float
-    acceleration_z: float
-    gyro_x: float
-    gyro_y: float
-    gyro_z: float
-    magnetometer_x: float
-    magnetometer_y: float
-    magnetometer_z: float
-    pressure: float
-    coarse_id: Optional[conint(ge=0, le=1000)] = Field(default=0, description="Coarse ID value from 0 to 8")
-
-    @validator('time')
-    def validate_time(cls, v):
-        if v < 0:
-            raise ValueError("Time must be a positive Unix timestamp")
-        return v
+    time: int = Field(..., description="Unix timestamp", ge=0)
+    acceleration_x: Optional[float] = None
+    acceleration_y: Optional[float] = None
+    acceleration_z: Optional[float] = None
+    gyro_x: Optional[float] = None
+    gyro_y: Optional[float] = None
+    gyro_z: Optional[float] = None
+    magnetometer_x: Optional[float] = None
+    magnetometer_y: Optional[float] = None
+    magnetometer_z: Optional[float] = None
+    pressure: Optional[float] = None
+    label_id: Optional[int] = Field(default=None, description="Label ID value from 0 to 8", ge=0, le=8)
 
     class Config:
-        allow_mutation = False  # Поля неизменяемы, кроме coarse_id
+        allow_mutation = False  # Поля неизменяемы, кроме label_id
 
-    def set_coarse_id(self, value: int):
+    def set_label_id(self, value: int):
         if not 0 <= value <= 8:
-            raise ValueError("Coarse ID value must be between 0 and 8")
-        object.__setattr__(self, 'coarse_id', value)
+            raise ValueError("Label ID value must be between 0 and 8")
+        object.__setattr__(self, 'label_id', value)
--- app/schemas.py
+++ app/schemas.py
@@ -2,15 +2,15 @@ from sqlalchemy import Column, Float, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
 
-class CoarseSchema(Base):
-    __tablename__ = "coarses"
+class LabelSchema(Base):
+    __tablename__ = "labels"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=False)  # No auto-increment to restrict IDs 0-8
     name = Column(String, nullable=False)
 
     # Опционально: определение строкового представления для удобства отладки
     def __repr__(self):
-        return f"<Coarse(id={self.id}, name={self.name})>"
+        return f"<Label(id={self.id}, name={self.name})>"
 
 class MotionSchema(Base):
     __tablename__ = "motions"
@@ -27,12 +27,12 @@ class MotionSchema(Base):
     magnetometer_y = Column(Float, nullable=True)
     magnetometer_z = Column(Float, nullable=True)
     pressure = Column(Float, nullable=True)
-    coarse_id = Column(Integer, ForeignKey(f'{CoarseSchema.__tablename__}.id'), nullable=True)
+    label_id = Column(Integer, ForeignKey(f'{LabelSchema.__tablename__}.id'), nullable=True)
 
-    # Связь с моделью Coarse
-    coarse = relationship("Coarse")
+    # Связь с моделью LabelSchema
+    label = relationship("LabelSchema")
 
-    def set_coarse_id(self, value: int):
+    def set_label_id(self, value: int):
         if not 0 <= value <= 8:
-            raise ValueError("Coarse ID must be between 0 and 8")
-        self.coarse_id = value
+            raise ValueError("Label ID must be between 0 and 8")
+        object.__setattr__(self, 'label_id', value)
